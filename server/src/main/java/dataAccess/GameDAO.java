e5416fe1c7a18167f0e092d7a767f3e1d5d07440
Kevin Young <kevindyoung76@gmail.com>
2024-02-26 15:20:20 -0700

    Created Services files

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -19,3 +19,20 @@ This exception should be thrown by data access methods that could fail. If a met
 it should throw a DataAccessException. For example, the DataAccessException is thrown if a user
 attempts to update a non-existent game.
 */
+
+/*
+Example Data Access Methods
+Here are some examples of the kinds of methods your DAOs will need to support. This list is not exhaustive. You should consult your server design in order to determine all of the methods you need to provide.
+
+clear: A method for clearing all data from the database. This is used during testing.
+createUser: Create a new user.
+getUser: Retrieve a user with the given username.
+createGame: Create a new game.
+getGame: Retrieve a specified game with the given game ID.
+listGames: Retrieve all games.
+updateGame: Updates a chess game. It should replace the chess game string corresponding to a given gameID. This is used when players join a game or when a move is made.
+createAuth: Create a new authorization.
+getAuth: Retrieve an authorization given an authToken.
+deleteAuth: Delete an authorization so that it is no longer valid.
+
+ */
