83641a3bf738d883d15642030227de7ce7e8c001
Kevin Young <kevindyoung76@gmail.com>
2024-02-26 14:59:43 -0700

    Initial project setup + creation of records

---
+++ shared/src/main/model/GameData.java
@@ -0,0 +1,23 @@
+package chess;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    GameData setGameID(int newID){
+        return new GameData(newID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    GameData setWhiteUsername(String newWhiteUsername){
+        return new GameData(gameID, newWhiteUsername, blackUsername, gameName, game);
+    }
+
+    GameData setBlackUsername(String newBlackUsername){
+        return new GameData(gameID, whiteUsername, newBlackUsername, gameName, game);
+    }
+
+    GameData setGameName(String newGameName){
+        return new GameData(gameID, whiteUsername, blackUsername, newGameName, game);
+    }
+
+    GameData setGame(ChessGame newGame){
+        return new GameData(gameID, whiteUsername, blackUsername, gameName, newGame);
+    }
+}
