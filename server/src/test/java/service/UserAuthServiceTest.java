5e8567e555c03d553ee17781c3a0a4f4d11cc094
Kevin Young <kevindyoung76@gmail.com>
2024-02-27 18:25:09 -0700

    registerTests

--- server/src/test/java/service/UserAuthServiceTest.java
+++ server/src/test/java/service/UserAuthServiceTest.java
@@ -1,8 +1,10 @@
 package service;
 
+import Exceptions.UserAlreadyExistsException;
 import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.Test;
+import server.Server;
 
 import java.util.HashSet;
 
@@ -11,12 +13,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserAuthServiceTest {
 
     @Test
-    void register() {
+    void registerInserts() throws UserAlreadyExistsException {
         HashSet<AuthData> authDatabase = new HashSet<AuthData>();
         HashSet<UserData> userDatabase = new HashSet<UserData>();
 
+        UserAuthService userAuthService = new UserAuthService();
+        GameService gameService = new GameService();
+
+        UserData userData = new UserData("username","password","email");
+        userAuthService.register(userData);
+
         assertNotNull(authDatabase);
         assertNotNull(userDatabase);
+    }
+
+    @Test
+    void registerInvalid() throws UserAlreadyExistsException {
+        HashSet<AuthData> authDatabase = new HashSet<AuthData>();
+        HashSet<UserData> userDatabase = new HashSet<UserData>();
+
+        UserAuthService userAuthService = new UserAuthService();
+        GameService gameService = new GameService();
+
+        UserData userData = new UserData("username","password","email");
+        userAuthService.register(userData);
+
+        assertThrows(UserAlreadyExistsException.class, () -> userAuthService.register(userData));
     }
 
     @Test
@@ -28,6 +50,7 @@ class UserAuthServiceTest {
     }
 
     @Test
-    void clearAll() {
+    void clearAllDatabase() {
     }
+
 }
