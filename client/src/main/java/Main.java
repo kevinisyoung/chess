b2d75d1f5f5c94dc4d60a7a3056b1c73aa4241bd
Kevin Young <kevindyoung76@gmail.com>
2024-03-25 16:24:41 -0600

    login/register collect user info

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,18 +5,73 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean isLoggedIn = false;
+        String username = "";
+
 
         while (!isLoggedIn) {
-            System.out.printf("\nWelcome to chess. Awaiting your input now, type \"help\" for possible commands.\n");
+            System.out.println("\nWelcome to chess. Awaiting your input now, type \"help\" for possible commands.");
             Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine();
+            String userInput = scanner.next();
             userInput = userInput.toLowerCase();
             String responseOutput = "";
+
+            String password = null;
+            String email = null;
 
             switch (userInput){
                 case "help": responseOutput = "--HELP MENU:--\n\"Register\": Create account\n\"Login\": Log into account\n\"Quit\": Exit the program";
 			    break;
                 case "login":
+                    System.out.print("Please enter <username> <password>");
+                    username = scanner.next();
+                    password = scanner.next();
+                    responseOutput = "Username: " + username + ", Password: " + password;
+                    System.out.println("\n\n\n\n\n\n\n");
+                    // perform login function
+                    isLoggedIn = true;
+                    break;
+                case "register":
+                    System.out.println("Enter the following information: <username> <password> <email>");
+                    username = scanner.next();
+                    password = scanner.next();
+                    email = scanner.next();
+                    responseOutput = username + password + email;
+                    break;
+	        }
+            System.out.println(responseOutput);
+
+        }
+        while (isLoggedIn) {
+            System.out.println("\nWelcome, " + username + ". Awaiting your input now, type \"help\" for possible commands.");
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()){
+                scanner.skip(".*");
+            }
+            String userInput = scanner.nextLine();
+            userInput = userInput.toLowerCase();
+            String responseOutput = "";
+
+            String password;
+            String email;
+
+            switch (userInput){
+                case "help": responseOutput = "--HELP MENU:--\n\"Register\": Create account\n\"Login\": Log into account\n\"Quit\": Exit the program";
+			    break;
+                case "login":
+                    System.out.println("Enter the following information: <username> <password>");
+                    username = scanner.next();
+                    password = scanner.next();
+                    responseOutput = username + password;
+                    //perform login function
+                    isLoggedIn = true;
+                    break;
+                case "register":
+                    System.out.println("Enter the following information: <username> <password> <email>");
+                    username = scanner.next();
+                    password = scanner.next();
+                    email = scanner.next();
+                    responseOutput = username + password + email;
+                    break;
 	        }
             System.out.println(responseOutput);
 
